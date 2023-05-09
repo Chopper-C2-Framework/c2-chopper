2abe6b08141dd98b38507685e0c9bfc959b758f0
M0ngi <saidanemongi@gmail.com>
2023-05-09 20:40:15 +0100

    Add User-Team relation

--- server/domain/entity/team.go
+++ server/domain/entity/team.go
@@ -6,5 +6,6 @@ import (
 
 type TeamModel struct {
 	gorm.Model
-	name string
+	name    string
+	members []*UserModel `gorm:"many2many:user_team;"`
 }
--- server/domain/entity/user.go
+++ server/domain/entity/user.go
@@ -8,4 +8,5 @@ type UserModel struct {
 	gorm.Model
 	username string
 	password string
+	teams    []*TeamModel `gorm:"many2many:user_team;"`
 }
