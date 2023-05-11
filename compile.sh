feb9b01171c378b10d99219adde5b0f2fd72b544
yassinebk <yassinebk23@gmail.com>
2023-05-11 20:33:40 +0100

    feat: fixed compile.sh now we can genreate both http and grpc stubs

--- compile.sh
+++ compile.sh
@@ -1,10 +1,19 @@
 export PATH="$PATH:/usr/local/go/bin:$HOME/go/bin"
 
 for file in $(find ./grpc/proto-files -name "*.proto"); do
-	protoc --go_out=paths=source_relative:./grpc/proto \
+	protoc -I grpc/proto/google/api \
+        --go_out=paths=source_relative:./grpc/proto \
 		--go-grpc_out=paths=source_relative:./grpc/proto \
 		--proto_path=./grpc/proto-files \
+        --grpc-gateway_out ./grpc/proto \
+        --grpc-gateway_opt logtostderr=true \
+        --grpc-gateway_opt paths=source_relative \
+        --grpc-gateway_opt generate_unbound_methods=true \
 		$file
+
+
+
+
 
 	# protoc --go-grpc_out=Mclient:client/grpc/ \
 	# 	--go-grpc_out=Mserver:server/grpc/ \
