7b49aaf92462593984ef83a210444c8ddf693403
M0ngi <saidanemongi@gmail.com>
2023-05-21 18:38:37 +0100

    Add type to task model

--- core/domain/entity/task.go
+++ core/domain/entity/task.go
@@ -1,11 +1,30 @@
 package entity
 
+import "database/sql/driver"
+
+type TaskType string
+
+const (
+	Ping      TaskType = "ping"
+	Shell     TaskType = "shell"
+	LiveShell TaskType = "live-shell"
+)
+
 type TaskModel struct {
 	UUIDModel
 	Name      string
-	Repeat    bool
+	Type      TaskType `json:"type" sql:"type:ENUM('ping', 'shell', 'live-shell')"`
 	AgentId   int
 	Agent     AgentModel
 	CreatorId int
 	Creator   UserModel `gorm:"foreignKey:CreatorId"`
+}
+
+func (e *TaskType) Scan(value interface{}) error {
+	*e = TaskType(value.([]byte))
+	return nil
+}
+
+func (e TaskType) Value() (driver.Value, error) {
+	return string(e), nil
 }
