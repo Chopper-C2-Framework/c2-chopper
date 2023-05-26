a6b3f5b4ef2587ac468c85079de94e2da22081cf
M0ngi <saidanemongi@gmail.com>
2023-05-26 21:20:59 +0100

    Add agent template code

--- agent/main.go
+++ agent/main.go
@@ -18,6 +18,18 @@ import (
 const UUID_FILE = "uuid.test"
 const HOST = "localhost:9002"
 
+type Services struct {
+	AgentService pb.AgentServiceClient
+	TaskService  pb.TaskServiceClient
+}
+
+type AgentInfo struct {
+	Uuid     string
+	Username string
+	UserId   string
+	Hostname string
+}
+
 func loadUUID() (string, error) {
 	file, err := os.OpenFile(UUID_FILE, os.O_RDONLY, 0644)
 	if err != nil {
@@ -55,40 +67,37 @@ func updateUUID(uuid string) {
 	os.WriteFile(UUID_FILE, []byte(uuid), 0644)
 }
 
-func main() {
+func gRPCConnect() {
+
+}
+
+func Connect(conn *grpc.ClientConn, services *Services) *AgentInfo {
 	uuid, _ := loadUUID()
 	user, _ := user.Current()
+	username := user.Username
+	userId := user.Uid
 	hostname, _ := os.Hostname()
 
-	fmt.Println(uuid, user.Username, user.Uid)
+	fmt.Println(uuid, username, userId)
 	fmt.Println("Hostname:", hostname)
-
-	conn, err := grpc.Dial(HOST, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Panic("error connecting:", err)
-		return
-	}
-	defer conn.Close()
-
-	client := pb.NewAgentServiceClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
 
 	agent := &pb.Agent{
 		Hostname: hostname,
-		Username: user.Username,
-		UserId:   user.Uid,
+		Username: username,
+		UserId:   userId,
 	}
 
 	if len(uuid) != 0 {
 		agent.Id = uuid
 	}
 
-	response, err := client.Connect(ctx, &pb.AgentConnectionRequest{Data: agent})
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := services.AgentService.Connect(ctx, &pb.AgentConnectionRequest{Data: agent})
 	if err != nil {
 		log.Panic("error registering connection:", err)
-		return
+		return nil
 	}
 
 	newUUID := response.GetUuid()
@@ -97,5 +106,88 @@ func main() {
 	if newUUID != uuid {
 		uuid = newUUID
 		updateUUID(newUUID)
+	}
+
+	return &AgentInfo{
+		Uuid:     uuid,
+		Username: username,
+		UserId:   userId,
+		Hostname: hostname,
+	}
+}
+
+func InitServices(conn *grpc.ClientConn) *Services {
+	AgentService := pb.NewAgentServiceClient(conn)
+	TaskService := pb.NewTaskServiceClient(conn)
+	return &Services{
+		AgentService: AgentService,
+		TaskService:  TaskService,
+	}
+}
+
+func FetchTasks(services *Services, info *AgentInfo) ([]*pb.Task, uint32, error) {
+	request := &pb.GetAgentTasksRequest{
+		AgentId: info.Uuid,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	resp, err := services.TaskService.GetAgentTasks(ctx, request)
+	if err != nil {
+		return nil, 0, err
+	}
+	return resp.GetTasks(), resp.GetSleepTime(), nil
+}
+
+func ExecuteTask(task *pb.Task) (*pb.TaskResult, error) {
+	// Handle execution & stuff
+	return &pb.TaskResult{TaskId: task.TaskId}, nil
+}
+
+func SendResult(services *Services, result *pb.TaskResult) error {
+	request := &pb.CreateTaskResultRequest{
+		TaskResult: result,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err := services.TaskService.CreateTaskResult(ctx, request)
+	return err
+}
+
+func main() {
+	conn, err := grpc.Dial(HOST, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Panic("error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	services := InitServices(conn)
+
+	info := Connect(conn, services)
+
+	for {
+		tasks, sleep, err := FetchTasks(services, info)
+		if err != nil {
+			log.Panic("Unable to fetch tasks")
+		}
+
+		for _, task := range tasks {
+			// This can become multithreaded in the future
+			// But will require sync between { SendResult, Sleep } blocks
+			result, err := ExecuteTask(task)
+			if err != nil {
+				log.Panic("Unable to execute task")
+			}
+
+			err = SendResult(services, result)
+			if err != nil {
+				log.Panic("Unable to submit task result")
+			}
+
+			time.Sleep(time.Duration(sleep * uint32(time.Second)))
+		}
+
 	}
 }
