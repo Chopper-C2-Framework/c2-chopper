55cbd85a62fea5d1f45e6b0018c3df70213b42c9
yassinebk <yassinebk23@gmail.com>
2023-05-29 07:22:16 +0100

    fix: correct http port

--- client/frontend/src/lib/get-server-url.ts
+++ client/frontend/src/lib/get-server-url.ts
@@ -1,6 +1,6 @@
 export const getServerUrl = (): string => {
   console.log(import.meta.env);
   return import.meta.env.DEV
-    ? "http://localhost:9003/v1"
+    ? "http://localhost:4000/v1"
     : (import.meta.env.VITE_API_URL as string);
 };
